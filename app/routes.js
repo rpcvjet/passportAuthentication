bf27ed588b8a247b651e0b22d77e4a73bfa448be
Kenneth Ashley Edwards <kenneth.ashley@gmail.com>
2017-03-24 21:24:40 -0700

    redirect to profile page working-

--- app/routes.js
+++ app/routes.js
@@ -33,7 +33,11 @@ module.exports = function(app, passport) {
   });
 
   // process the signup form
-  // app.post('/signup', do all our passport stuff here);
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  }));
 
   // =====================================
   // PROFILE SECTION =====================
@@ -41,6 +45,7 @@ module.exports = function(app, passport) {
   // we will want this protected so you have to be logged in to visit
   // we will use route middleware to verify this (the isLoggedIn function)
   app.get('/profile', isLoggedIn, function(req, res) {
+    console.log('i\m in the profile section');
     res.render('profile.ejs', {
       user : req.user // get the user out of session and pass to template
     });
