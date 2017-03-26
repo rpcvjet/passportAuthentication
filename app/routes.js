815c099fbe84d82b1fa14678eec8228bff7e06a3
Kenneth Ashley Edwards <kenneth.ashley@gmail.com>
2017-03-25 23:14:04 -0700

    added login code, redirects correctly

--- app/routes.js
+++ app/routes.js
@@ -20,7 +20,11 @@ module.exports = function(app, passport) {
   });
 
   // process the login form
-  // app.post('/login', do all our passport stuff here);
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/login', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  }));
 
   // =====================================
   // SIGNUP ==============================
@@ -35,7 +39,7 @@ module.exports = function(app, passport) {
   // process the signup form
   app.post('/signup', passport.authenticate('local-signup', {
     successRedirect : '/profile', // redirect to the secure profile section
-    failureRedirect : '/', // redirect back to the signup page if there is an error
+    failureRedirect : '/signup', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
   }));
 
